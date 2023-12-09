d6948078746e4780372e8dda47b3fc58b6ed4832
avishaigonen123 <avishaigonen@gmail.com>
2023-12-09 22:22:54 +0200

    changed that the unread messages would work

--- forward-translate-bot.py
+++ forward-translate-bot.py
@@ -31,6 +31,7 @@ async def translate_and_forward(event):
         #print(f"Translated Text: {translated_text}")  # Print the translated text for debugging
         message.text = translated_text
     await client.send_message(destination_channel_id, message)
+    await client.send_read_acknowledge(await findGroup(source_channel_id),max_id=0) # i want it to clear the unread messages
 
 
                   
@@ -40,7 +41,6 @@ async def findGroup(GroupID: int):
             return i
         
 
-
 async def main():
     translator = Translator()
 
@@ -49,18 +49,18 @@ async def main():
 
     if source_channel.unread_count!=0:
         count=source_channel.unread_count
-        mess=[]
-        async for i in client.iter_messages(source_channel,count):
-            if not (i.action):#patched.Message.
-                    mess+=[i]
-        mess.reverse()
-        if len(mess)>0:
-           for i in mess:
-                sendMes=i
+        messageList=[]
+        async for message in client.iter_messages(source_channel,count):
+            if not (message.action):#patched.Message.
+                    messageList+=[message]
+        messageList.reverse()
+        if len(messageList)>0:
+           for message in messageList:
+                sendMes=message
                 if(sendMes.text):
                     sendMes.text=translator.translate(sendMes.text, dest='he').text
                 await client.send_message(destination_channel,sendMes)
-                await client.send_read_acknowledge(source_channel,max_id=i.id)
+                await client.send_read_acknowledge(source_channel,max_id=message.id)
 
     #else: print("not found new messages")
     
@@ -70,9 +70,8 @@ async def main():
 
 
 client.start("+972585328077") # here enter your phone number, in the first time you would need to give the code from telegram, each time you create a new session
-
 # Run the main function once before starting the event loop
 client.loop.run_until_complete(main()) # i want that first it will send all the unread messages, and then it will use the event mechanic.
 
 # Start the userbot and run the event loop
-client.run_until_disconnected()
+client.run_until_disconnected() 

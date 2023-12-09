53f5660e54e613e0775d9cec8922fbcc1d6e1a50
avishaigonen123 <avishaigonen@gmail.com>
2023-12-09 20:16:35 +0200

    two batch files for running and terminating the bot. needs to use the auto-py-to-exe.exe file inorder to create an exe file from the python script, and then changing stuff in the batch files.

---
+++ runBot.bat
@@ -0,0 +1,12 @@
+@echo off
+set "process_name=forward-translate-bot.exe"
+
+tasklist /FI "IMAGENAME eq %process_name%" 2>NUL | findstr /I /C:"%process_name%" >NUL
+
+if errorlevel 1 (
+    echo No existing instance found. Starting a new process.
+    cd /d C:\Users\avish\output\forward-translate-bot
+    start /B C:\Users\avish\output\forward-translate-bot\forward-translate-bot.exe
+) else (
+    echo An instance is already running. No new process started.
+)

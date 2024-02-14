fe85f0a85ffd5809afece2d085f95e585b701cae
avishaigonen123 <avishaigonen@gmail.com>
2024-02-14 16:04:55 +0200

    modify the script, chnaged some bat files and vbs files

--- README.md
+++ README.md
@@ -8,11 +8,9 @@ and forward this into my channel :)
 Here you enter to get the api data of your user.
 https://my.telegram.org/auth?to=apps
 
-If you want to use the batch files easing your process of runing, you need to download auto-py-to-exe:
-pip install auto-py-to-exe
-and then, changing the directories in the runBot.bat file and also in the killBot.bot. 
-notice also to change the in the "cd" line in the runBot.bat the path of your exe file, where the session is being created.
+you need to change the path to your python, in the run_bot.bat file.
 
+i added vbscript files, to run the script without command window.
 
 
 #פלסטיןחינם
--- forward-translate-bot.py
+++ forward-translate-bot.py
@@ -1,8 +1,9 @@
+
 from googletrans import Translator
 from telethon import TelegramClient, events, types
 
 # Replace '22426045' and 'e7b8ee99e8bf9c36984d18f1baae5591' with your actual API ID and API hash
-api_id = 22426045   
+api_id = 22426045
 api_hash = 'e7b8ee99e8bf9c36984d18f1baae5591'
 
 # Replace 'mySession' with a unique session name
@@ -14,25 +15,38 @@ source_channel_id = -1001300814792
 # Replace '-1002027365084' with the actual chat ID of your bot's channel
 destination_channel_id = -1002027365084
 
+in_translate_process = False
+
 # Create a TelegramClient instance
 client = TelegramClient(session_name, api_id, api_hash)
 
 @client.on(events.NewMessage(chats=source_channel_id))
 async def translate_and_forward(event):
-    message = event.message
-    if message.text:  # maybe there would be a picture without text
-        #print(f"Translating and forwarding message with ID: {message.id}.")
-        #print(message.text)
-        # Translate the message text to Hebrew
-        translator = Translator()
-        # Use message.text directly
-        translated_text = translator.translate(message.text, dest='he').text
+    global in_translate_process   # a simple lock
+    if not in_translate_process:
+        in_translate_process = True
+        translator = Translator()   
 
-        #print(f"Translated Text: {translated_text}")  # Print the translated text for debugging
-        message.text = translated_text
-    await client.send_message(destination_channel_id, message)
-    await client.send_read_acknowledge(await findGroup(source_channel_id),max_id=0) # i want it to clear the unread messages
+        source_channel = await findGroup(source_channel_id)
+        destination_channel = await findGroup(destination_channel_id)
 
+        if source_channel.unread_count!=0:
+            count=source_channel.unread_count
+
+            mess=[]
+            async for i in client.iter_messages(source_channel,count):
+                if not (i.action): # and i not in mess:#patched.Message.
+                    mess.insert(0,i)
+                    print(i)
+
+            if len(mess)>0:
+                for i in mess:
+                        sendMes=i
+                        if(sendMes.text):
+                            sendMes.text=translator.translate(sendMes.text, dest='he').text
+                        await client.send_message(destination_channel,sendMes)
+                        await client.send_read_acknowledge(source_channel,max_id=i.id)
+        in_translate_process = False
 
                   
 async def findGroup(GroupID: int):
@@ -40,6 +54,7 @@ async def findGroup(GroupID: int):
         if i.id==GroupID:
             return i
         
+
 
 async def main():
     translator = Translator()
@@ -49,29 +64,29 @@ async def main():
 
     if source_channel.unread_count!=0:
         count=source_channel.unread_count
-        messageList=[]
-        async for message in client.iter_messages(source_channel,count):
-            if not (message.action):#patched.Message.
-                    messageList+=[message]
-        messageList.reverse()
-        if len(messageList)>0:
-           for message in messageList:
-                sendMes=message
-                if(sendMes.text):
-                    sendMes.text=translator.translate(sendMes.text, dest='he').text
-                await client.send_message(destination_channel,sendMes)
-                await client.send_read_acknowledge(source_channel,max_id=message.id)
+        mess=[]
+        async for i in client.iter_messages(source_channel,count):
+            if not (i.action): #and i not in mess:#patched.Message.
+                mess.insert(0,i)
+
+        if len(mess)>0:
+            for i in mess:
+                    sendMes=i
+                    if(sendMes.text):
+                        sendMes.text=translator.translate(sendMes.text, dest='he').text
+                    await client.send_message(destination_channel,sendMes)
+                    await client.send_read_acknowledge(source_channel,max_id=i.id)
 
     #else: print("not found new messages")
     
     #translation = translator.translate("Der Himmel ist blau und ich mag Bananen", dest='he')
     #print(translation.text)
+
     
 
 
-client.start("+972585328077") # here enter your phone number, in the first time you would need to give the code from telegram, each time you create a new session
+client.start("+972585328077")
 # Run the main function once before starting the event loop
 client.loop.run_until_complete(main()) # i want that first it will send all the unread messages, and then it will use the event mechanic.
-
 # Start the userbot and run the event loop
-client.run_until_disconnected() 
+client.run_until_disconnected()
